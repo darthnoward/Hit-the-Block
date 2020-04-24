3a326ae0ed8aeb7205bea9da284e2fb6fbb54c9b
darthnoward <me@darthnoward.com>
2020-04-25 05:16:34 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# Hit the Block
+A game with graphic interface that allows users to control the paddle with mouse to deflect the ball so as to hit as many blocks as possible in order to keep the ball from hitting the bottom as well as to get to next level.
+
+It serves as the final assignment of 10.009 Digital World.
+
+![appearance](./assets/screenshot.jpg)
+
+## Dependency
+
+- Python
+- Kivy
+- Libdw
+
+and the dependencies of all above
+
+## Run the game 
+
+After fulfill the dependencies, in the cloned directory, in your favorite shell:
+```shell
+python3 main.py 
+```
+
+## Play the game 
+
+### Basic rules
+
+- To start, click the *START* button.
+- drag the cyan-color paddle or the bottom half of the screen (but above the frame ui), and try your best to deflect the ball to hit the blocks.
+- After clearing the level and prompted with *'LEVEL PASSED'*, click the *START* button to proceed to next level.
+- If you want to restart the level during game, click the *STOP* button.
+
+### Additional Informations
+
+- Each time the ball hits either the paddle or the ceiling, the velocity on y axis will raise by 0.03x.
+- If the velocity gets too high (travels beyond the border in a single frame), it counts as lost. Although your eyes probably will not be able to follow that fast a thing.
+- Even the very sides of the paddle may not completely offset the velocity of the ball on x axis, so manage it carefully.
+- Practice makes perfect.
+
+## The code
+
+### class MainApp
+> The class which the app is instantiated. 
+
+- loads kivy language with kivy Builder, which describes initial user interface and interactions.
+- set the root widget to be updated 65 times per second to achieve stable 60hz refresh rate.
+- return the root widget.
+
+### class MainGame
+> subclass of kivy App, root widget of the game 
+
+- instantiate an Velocity_sm instance to track game level and ball initial velocity.
+- declare the classes described in kivy language.
+- function update:
+    - resizing window to avoid display bugs from resizing window in the progress
+    - call the move function of the ball base on its velocity
+    - call bounce_ball function of the paddle 
+    - check if the blocks are hit and if the level is cleared
+    - adjust the velocity of the ball depending on its position
+- function start:
+    - centralize the ball and paddle
+    - set the initial velocity of the ball base on level of the game 
+    - update the appearance of the button and the label
+- function stop:
+    - reset the ball velocity to 0
+    - reset all the blocks 
+    - update the appearance of the button and the label
+
+### class Velocity_sm
+> subclass of libdw.sm, get track of game level and Velocity 
+
+- Instantiated when game is instantiated
+- upon proceeding to next level, current level and velocity are provided as arguments (state and input) to get the next state value.
+
+### class StartButton
+> subclass of kivy Button
+
+- bound to function toggle to toggle between gaming and stopped state.
+
+### class Spacing 
+> subclass of kivy widget 
+
+- acts as compartment to separate the blocks in the BoxLayout. 
+- has the same color as the background
+
+## Known issues
+
+- The BoxLayout gets bugged out when attempting to zoom out the window, leaving misplaced block canvas. Window resizing is thus blocked as for now.
+
+## LICENSE
+Licensed under the [MIT license](./LICENSE)
---
+++ main.py
@@ -0,0 +1,391 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.graphics import Rectangle, Color
+from kivy.properties import ListProperty, NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+from kivy.core.window import WindowBase, Window
+from kivy.lang import Builder
+ 
+from libdw import sm
+
+from time import sleep
+
+class Velocity_sm(sm.SM):
+	def __init__(self, velocity):
+		super(Velocity_sm, self).__init__()
+		self.velocity = velocity
+		
+	start_state = 1
+	
+	def get_next_value(self, state, inp):
+		next_state = state + 1
+		output = inp * (1 + 1.0 / next_state)
+		return next_state, output
+
+class MainGame(Widget):
+	v = Velocity_sm(3)
+	v.start()
+	lvl = NumericProperty(v.state)
+	
+	label = ObjectProperty(None)
+	ball = ObjectProperty(None)
+	ball = ObjectProperty(None)
+	paddle = ObjectProperty(None)
+	button = ObjectProperty(None)
+	box1 = ObjectProperty(None)
+	box2 = ObjectProperty(None)
+		
+	rect1 = ObjectProperty(None)
+	rect2 = ObjectProperty(None)
+	rect3 = ObjectProperty(None)
+	rect4 = ObjectProperty(None)
+	rect5 = ObjectProperty(None)
+	rect6 = ObjectProperty(None)
+	rect7 = ObjectProperty(None)
+	rect8 = ObjectProperty(None)
+	rect9 = ObjectProperty(None)
+	rect10 = ObjectProperty(None)
+	rect11 = ObjectProperty(None)
+	rect12 = ObjectProperty(None)
+	rect13 = ObjectProperty(None)
+	rect14 = ObjectProperty(None)
+	rect15 = ObjectProperty(None)
+
+	# on_click callback of the button
+	def toggle(self):
+		if self.ball.velocity != Vector(0, 0):
+			self.ball.center = self.center[0], self.center[1] + 60
+			self.stop()
+		else:
+			self.start()
+			
+	def stop(self):
+		self.ball.velocity = Vector(0, 0)
+		
+		rectlist = [self.rect1, self.rect2, self.rect3, self.rect4, self.rect5, self.rect6, self.rect7, self.rect8, self.rect9, self.rect10, self.rect11, self.rect12, self.rect13, self.rect14, self.rect15]
+		for rect in rectlist:
+			rect.reset()
+
+		self.button.text = "START"
+		self.button.background_color = (0.1, 1, 0, 0.9)
+		self.paddle.center_x = self.center_x
+		
+	def start(self):
+		self.paddle.center_x = self.center_x
+		self.ball.velocity = Vector(0, -1 * self.v.velocity)
+		self.button.text = "STOP"
+		self.label.color = (1,1,1,1)
+		self.label.text = "Level: " + str(self.v.state)
+		self.button.background_color = (1, 0, 0, 0.6)
+	
+	def update(self, dt):
+			if Window.size != (1000, 900):
+				Window.size = (1000, 900)
+			
+			self.ball.move()
+			self.paddle.bounce_ball(self.ball)
+			
+			rectlist = [self.rect1, self.rect2, self.rect3, self.rect4, self.rect5, self.rect6, self.rect7, self.rect8, self.rect9, self.rect10, self.rect11, self.rect12, self.rect13, self.rect14, self.rect15]
+			counter = 0
+			
+			# loop to check if targets are hit, if not proceed with bounce_ball function
+			for i in range(len(rectlist)):
+				if rectlist[i].bounce == 0:
+					rectlist[i].bounce_ball(self.ball)
+				else:
+					counter += 1
+					
+			# every targets are hit, enter the next level
+			if counter == i + 1:
+				self.ball.center = self.center[0], self.center[1] + 60
+				self.stop()
+				self.label.text = "LEVEL PASSED!"
+				self.label.color = (0,1,0,1)
+				
+				self.v.state, self.v.velocity = self.v.get_next_value(self.v.state, self.v.velocity)
+			
+			# hit top, add y velocity as penalty
+			if self.ball.top >= self.height - 5:
+				self.ball.velocity_y *= -1 
+				self.ball.velocity_y -=  0.3 * self.v.state**0.5
+			
+			# hit left or right
+			if (self.ball.x <= 5) or (self.ball.right >= self.width - 5):
+				self.ball.velocity_x *= -1
+			
+			# hit bottom bar, game over, reset level
+			if self.ball.y <= 160:
+				self.label.text = "GAME OVER"
+				self.label.color = (1,0,0,0.9)
+				self.v = Velocity_sm(3)
+				self.v.start()
+				
+				self.ball.move()
+				sleep(0.2)
+				self.ball.center = self.center[0], self.center[1] + 60
+				self.stop()
+				
+	def on_touch_move(self, touch):
+			if touch.y < self.height / 2 + 160 and touch.y >= 130:
+				self.paddle.center_x = touch.x
+				
+class StartButton(Button):
+	pass
+
+# blank space between the targets
+class Spacing(Widget):
+	pass
+
+class Paddle(Widget):
+	def bounce_ball(self, ball):
+		if self.collide_widget(ball):
+			vx, vy = ball.velocity
+			offset = (ball.center_x - self.center_x) / (self.width / 10.0)
+			
+			bounced = Vector(vx, vy * -1)
+			ball.velocity = (bounced.x + offset) * 1.1, bounced.y * 1.03
+
+class Rect(Widget):
+	bounce = 0
+	
+	def reset(self):
+		self.bounce = 0
+		with self.canvas:
+			Color(1, 1, 0)
+			Rectangle(pos=self.pos, size=self.size)
+
+	def bounce_ball(self, ball):
+		if self.collide_widget(ball) and self.bounce == 0:
+			self.bounce += 1
+			
+			vx, vy = ball.velocity
+			offset = (ball.center_x - self.center_x) / self.width
+			bounced = Vector(vx, vy * -1)
+			vel = bounced * 1.03
+			ball.velocity = vel.x + offset, vel.y
+			
+			with self.canvas:
+				Color(0, 0, 0)
+				Rectangle(pos=self.pos, size=self.size)
+
+
+class Ball(Widget):
+	velocity_x = NumericProperty(0)
+	velocity_y = NumericProperty(0)
+	velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+	def move(self):
+		self.pos = Vector(*self.velocity) + self.pos 
+
+class MainApp(App):
+	def build(self):
+		
+		Builder.load_string("""
+<WindowBase>:
+	minimum_height: 1000
+	minimum_width: 900
+
+<MainGame>:    
+	canvas:
+		Color:
+			rgb: 1, 0, 0
+		Rectangle:
+			pos: 0, 155
+			size: self.width, 5
+		Color:
+			rgb:  0, 1, 0.7
+		Rectangle:
+			pos: 0, 0
+			size: 5, self.height
+		Color:
+			rgb:  0, 1, 0.7
+		Rectangle:
+			pos: root.width - 5, 0
+			size: 5, self.height
+		Color:
+			rgb:  1, 0.7, 0
+		Rectangle:
+			pos: 0, self.height - 5
+			size: self.width, 5
+		Color:
+			rgb: 1, 1, 1
+		Rectangle:
+			pos: 0, 130
+			size: self.width, 25
+			source: './assets/arrow.png'
+		Color:
+			rgb: 0.2, 0.25, 0.25
+		Rectangle:
+			pos: 0, 0
+			size: self.width, 130
+	
+	ball: ball
+	paddle: paddle
+	button: button
+	label: label
+	box1: box1
+	box2: box2
+	rect1: rect1
+	rect2: rect2
+	rect3: rect3
+	rect4: rect4
+	rect5: rect5
+	rect6: rect6
+	rect7: rect7
+	rect8: rect8
+	rect9: rect9
+	rect10: rect10
+	rect11: rect11
+	rect12: rect12
+	rect13: rect13
+	rect14: rect14
+	rect15: rect15
+
+	Paddle:
+		id: paddle
+		size: root.width / 8.0, root.height / 20.0
+		center_x: root.center_x - 81
+		y: root.y + 160
+		
+			
+	Label:
+		id: label
+		font_size: root.width * 0.05  
+		center_x: root.width / 4
+		top: root.top - root.height + 120
+		text: "Level: " + str(root.lvl)
+	
+	StartButton:
+		id: button
+		font_size: root.width * 0.05
+		x: root.width *  0.6
+		top: 100
+		text: "START"
+		size: root.width / 4.0, 130
+		background_color: 0.1, 1, 0, 0.6
+		on_press: root.toggle()
+		
+	BoxLayout:
+		id: box1
+		top: root.top
+
+		Rect:
+			id: rect1
+		Spacing:
+		Rect:
+			id: rect2
+		Spacing:
+		Rect:
+			id: rect3
+		Spacing:
+		Rect:
+			id: rect4
+		Spacing:
+		Rect:
+			id: rect5
+		Spacing:
+		Rect:
+			id: rect6
+		Spacing:
+		Rect:
+			id: rect7
+		Spacing:
+		Rect:
+			id: rect8
+		
+	BoxLayout:
+		id: box2
+		top: root.top - 50
+
+		Spacing:
+		Rect:
+			id: rect9 
+		Spacing:
+		Rect:
+			id: rect10
+		Spacing:
+		Rect: 
+			id: rect11
+		Spacing:
+		Rect: 
+			id: rect12
+		Spacing:
+		Rect: 
+			id: rect13
+		Spacing:
+		Rect: 
+			id: rect14
+		Spacing:
+		Rect: 
+			id: rect15
+		Spacing:
+		
+	Ball:
+		id: ball
+		center: self.parent.center
+
+<Ball>:
+	size: 50, 50
+	canvas:
+		Color:
+			rgb: 1, 0, 1
+		Ellipse:
+			pos: self.pos
+			size: self.size
+			
+<Paddle>:
+	canvas:
+		Color:
+			rgb: 1, 1, 1
+		Rectangle:
+			pos:self.pos
+			size:self.size
+			source: './assets/paddle.png'
+			
+<Rect>:
+	top: self.parent.top - 5
+	size:  self.parent.width / 8.0, 40
+	canvas:
+		Color:
+			rgb: 1, 1, 0
+		Rectangle:
+			pos:self.pos
+			size:self.size
+
+<Spacing>:
+	top: self.parent.top - 5
+	size: self.parent.width / 8.0, 40
+	size_hint_x: 0.5
+	canvas:
+		Color:
+			rgb: 0, 0, 0
+		Rectangle:
+			pos:self.pos
+			size:self.size
+			
+<BoxLayout>:
+	orientation: 'horizontal'
+	padding: 5
+	center_x: root.center_x
+	size: self.parent.width, 50
+		""")
+		
+		game = MainGame()
+		
+		game.box1.width = 1400
+		game.box2.width = 1400
+		game.ball.center = game.center[0], game.center[1] + 180
+		game.paddle.center_x = game.center_x
+		Clock.schedule_interval(game.update, 1 / 64.0)
+		
+		return game
+	
+if __name__ == '__main__':
+
+	Window.size = (1000,900)
+	
+	app = MainApp()
+	app.run()
